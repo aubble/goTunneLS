8e0d28fa4025e2d92579ad314f1f40853c4e674e
Anmol Sethi <anmol@aubble.com>
2015-09-02 19:49:04 -0400

    readme improvements

--- node.go
+++ node.go
@@ -249,7 +249,6 @@ func (n *node) server() error {
 // then copying all data between the two connections
 func (n *node) client() error {
 	var certPool *x509.CertPool
-	n.logln("swaggg")
 	if n.Cert != "" {
 		certPool = x509.NewCertPool()
 		raw, err := ioutil.ReadFile(n.Cert)
@@ -261,7 +260,6 @@ func (n *node) client() error {
 		if ok == false {
 			n.logln("could not append cert to RootCAs pool")
 		}
-	n.logln("swaggg")
 	}
 	host, _, err := net.SplitHostPort(n.Connect)
 	if err != nil {
